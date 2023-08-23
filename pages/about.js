a787db74fe54cddfad43ea64c8f0bf3a8f17ad06
Abir0248 <abir15-6789@diu.edu.bd>
2023-08-23 21:34:50 +0600

    updates

--- pages/about.js
+++ pages/about.js
@@ -6,8 +6,8 @@ export default function About() {
     <div className="container mx-auto p-6">
       <Header />
 
-      <section className="mt-6 mb-6 min-h-screen">
-        <h1 className="text-3xl font-bold mb-4">About the Blog</h1>
+      <section className="mt-6 mb-6">
+        <h1 className="text-3xl font-bold mb-4 text-center">About the Blog</h1>
         <p className="text-lg mb-4">
           Welcome to our Blog Application, a place where thoughts are shared,
           ideas are nurtured, and voices are heard. Our blog is dedicated to
@@ -15,14 +15,29 @@ export default function About() {
           from latest trends to age-old traditions.
         </p>
 
-        <h2 className="text-2xl font-bold mb-4">Our Mission</h2>
+        <h2 className="text-2xl font-bold mb-4  text-center">Our Mission</h2>
         <p className="text-lg mb-4">
           Our mission is to empower writers and readers alike. To give a
           platform for those who want to share, and an oasis for those who seek
           to learn or simply enjoy a good read.
         </p>
+      </section>
 
-        <h2 className="text-2xl font-bold mb-4">The Team</h2>
+      <section className="mt-6 mb-6 min-h-screen ">
+        <h2 className="text-2xl font-bold mb-4  text-center">
+          Why Choose Our Blog?
+        </h2>
+        <p className="text-lg mb-4">
+          We stand out from the rest for several reasons:
+        </p>
+        <ul className="list-disc ml-8 mb-4">
+          <li>Diverse range of topics catering to a wide audience</li>
+          <li>Expert opinions and deep dives into subjects</li>
+          <li>Engaging and fresh content updated regularly</li>
+          <li>An active community of readers and writers</li>
+        </ul>
+
+        <h2 className="text-2xl font-bold mb-4  text-center">The Team</h2>
         <p className="text-lg">
           Behind this platform is a team of passionate individuals, writers,
           developers, and creatives, who believe in the power of sharing
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@
 
 import { useEffect, useState } from "react";
 import axios from "axios";
-import Link from "next/link";
 import Header from "../components/header";
 import Footer from "../components/footer";
 
@@ -11,7 +10,7 @@ export default function Home() {
 
   useEffect(() => {
     axios
-      .get("https://basic-blog.teamrabbil.com/api/post-categories")
+      .get("https://basic-blog.teamrabbil.com/api/post-newest")
       .then((response) => {
         setBlogItems(response.data);
       })
@@ -24,23 +23,45 @@ export default function Home() {
     <div className="container mx-auto p-6">
       <Header />
 
-      <section className=" p-6 text-center   bg-offwhite  mx-auto">
+      <section className=" p-6 text-center  bg-offwhite w-full mx-auto ">
         <h1 className="text-3xl font-bold mb-4">Welcome to Our Blog!</h1>
         <p>Stay updated with our latest news and articles.</p>
       </section>
 
-      <section className=" min-h-screen p-6 bg-offwhite mx-auto">
-        <h2 className="text-2xl font-bold mb-4 text-center">Blog Categories</h2>
-        <div className="flex flex-col justify-center items-center">
+      <section className="p-6  bg-offwhite mx-auto ">
+        <h2 className="text-2xl font-bold mb-4 text-center">
+          Why Read Our Blog?
+        </h2>
+        <p className="mb-4">
+          Our blog offers unique insights into a range of topics. Whether you're
+          looking to stay updated with the latest trends, deepen your knowledge
+          about specific subjects, or simply find entertainment, we have
+          something for everyone. Our team of dedicated writers ensures that
+          every piece is well-researched, engaging, and insightful. Join our
+          community and discover the world through our eyes.
+        </p>
+      </section>
+
+      <section className="  bg-offwhite mx-auto">
+        <h2 className="text-2xl font-bold mb-4 text-center">
+          Latest Blog Posts
+        </h2>
+        <div className="flex flex-col justify-center p-6 mx-auto items-center">
           <ul className="grid grid-cols-2 gap-4 rounded-lg">
-            {blogItems.map((blog) => (
+            {blogItems.slice(0, 4).map((blog) => (
               <li
                 key={blog.id}
-                className="mb-4 p-4 border rounded-lg bg-burlywood"
+                className=" bg-burlywood text-white  mb-4 p-4 border rounded-lg"
               >
-                <h3 className="text-xl text-center text-white  mx-auto ">
-                  {blog.name}
+                <img
+                  src={blog.img}
+                  alt={blog.title}
+                  className="w-full h-64 object-cover rounded mb-4"
+                />
+                <h3 className="text-lg font-bold mb-2 text-center">
+                  {blog.title}
                 </h3>
+                <p className="text-sm">{blog.short}</p>
               </li>
             ))}
           </ul>
